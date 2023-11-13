f770d1740cc410bd351f7695ca7aade671d49da3
Micah Woodring <84886282+EliteMicah@users.noreply.github.com>
2023-11-13 12:27:11 -0800

    Update education_screen.java

--- java/education_screen.java
+++ java/education_screen.java
@@ -1,47 +1,48 @@
 package com.example.secret_messenger;
 
-import androidx.appcompat.app.AppCompatActivity;
-import android.widget.Spinner;
-import android.widget.ArrayAdapter;
-import android.os.Bundle;
+import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.view.View;
+import android.os.Bundle;
 import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class education_screen extends AppCompatActivity {
 
     ImageButton encoding;
     ImageButton home;
+    ImageButton settings;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.education_screen);
 
         //HOME BUTTON
-        home = findViewById(R.id.homeIcon);
+        home = findViewById(R.id.homeButton);
         home.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent i = new Intent(education_screen.this,MainActivity.class);
-                        startActivity(i);
-                    }
+                v -> {
+                    Intent i = new Intent(education_screen.this,MainActivity.class);
+                    startActivity(i);
                 }
         );
 
         //HOME BUTTON
-        encoding = findViewById(R.id.EncodingScreen);
+        encoding = findViewById(R.id.EncodeButton);
         encoding.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent i = new Intent(education_screen.this,encode_screen.class);
-                        startActivity(i);
-                    }
+                v -> {
+                    Intent i = new Intent(education_screen.this,encode_screen.class);
+                    startActivity(i);
                 }
         );
 
-
+        settings = findViewById(R.id.settingsButton);
+        settings.setOnClickListener(
+                v -> {
+                    Intent i = new Intent(education_screen.this,settings_screen.class);
+                    startActivity(i);
+                }
+        );
     }
 }
