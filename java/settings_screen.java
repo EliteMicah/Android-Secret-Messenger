ad43b44bbd70a69d4ce5853e39db2ae0d6e44a71
Micah Woodring <84886282+EliteMicah@users.noreply.github.com>
2023-11-12 18:02:14 -0800

    Update settings_screen.java

--- java/settings_screen.java
+++ java/settings_screen.java
@@ -1,1 +1,14 @@
+package com.example.secret_messenger;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class settings_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_screen);
+    }
+}
