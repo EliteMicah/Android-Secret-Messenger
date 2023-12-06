cd74b7cb5709c74fbcfc6195af7d073f0b319bdc
Micah Woodring <84886282+EliteMicah@users.noreply.github.com>
2023-12-06 12:35:17 -0800

    Update settings_screen.java

--- java/settings_screen.java
+++ java/settings_screen.java
@@ -1,20 +1,34 @@
 package com.example.secret_messenger;
 
-import static com.example.secret_messenger.R.*;
-
-import androidx.appcompat.app.AppCompatActivity;
+import static com.example.secret_messenger.R.id;
+import static com.example.secret_messenger.R.layout;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.Spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.security.KeyPair;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.util.Objects;
+
+import javax.crypto.SecretKey;
 
 public class settings_screen extends AppCompatActivity {
 
     ImageButton home;
     ImageButton encode;
+
+    private static final String PREF_SELECTED_SPINNER_POSITION = "selected_spinner_position";
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -22,8 +36,13 @@ public class settings_screen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(layout.settings_screen);
 
+        // Back Button Toolbar
+        androidx.appcompat.widget.Toolbar myToolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+
         //DROPDOWN MENU SETUP
-        Spinner staticSpinner = findViewById(id.spinner1);
+        Spinner staticSpinner = findViewById(id.spinner);
 
         // Create an ArrayAdapter using the string array and a default spinner
         ArrayAdapter<CharSequence> staticAdapter = ArrayAdapter
@@ -35,10 +54,40 @@ public class settings_screen extends AppCompatActivity {
         // Apply the adapter to the spinner
         staticSpinner.setAdapter(staticAdapter);
 
+        // Retrieve the saved position from SharedPreferences and set the spinner selection
+        SharedPreferences preferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+        int selectedPosition = preferences.getInt(PREF_SELECTED_SPINNER_POSITION, 0);
+        staticSpinner.setSelection(selectedPosition);
+
+        // Set a listener to save the selected position when the spinner item is changed
+        staticSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
+                // Save the selected position in SharedPreferences
+                SharedPreferences preferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putInt(PREF_SELECTED_SPINNER_POSITION, position);
+                editor.apply();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                // Not used, but required for interface implementation
+            }
+        });
+
+        Bundle extras = getIntent().getExtras();
+        KeyPair keyPair= (KeyPair) Objects.requireNonNull(extras).get("KeyPair");
+        PrivateKey privateKey = Objects.requireNonNull(keyPair).getPrivate();
+        PublicKey publicKey = keyPair.getPublic();
+        SecretKey secretkey=(SecretKey) extras.get("AESkey");
+
         home = findViewById(id.homeButton);
         home.setOnClickListener(
                 v -> {
                     Intent i = new Intent(settings_screen.this,MainActivity.class);
+                    i.putExtra("KeyPair", keyPair);
+                    i.putExtra("AESKey", secretkey);
                     startActivity(i);
                 }
         );
@@ -47,10 +96,23 @@ public class settings_screen extends AppCompatActivity {
         encode.setOnClickListener(
                 v -> {
                     Intent i = new Intent(settings_screen.this,encode_screen.class);
+                    i.putExtra("KeyPair", keyPair);
+                    i.putExtra("AESKey", secretkey);
                     startActivity(i);
                 }
         );
-
-
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        // If we got here, the user's action was not recognized.
+        // Invoke the superclass to handle it.
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
