6950df1ea0d92535c3e4bedce5bbad6c57366ab8
Micah Woodring <84886282+EliteMicah@users.noreply.github.com>
2023-12-06 12:34:26 -0800

    Update MainActivity.java

--- java/MainActivity.java
+++ java/MainActivity.java
@@ -1,16 +1,25 @@
 package com.example.secret_messenger;
 
-import static com.example.secret_messenger.R.*;
-
-import androidx.appcompat.app.AppCompatActivity;
+import static com.example.secret_messenger.R.id;
+import static com.example.secret_messenger.R.layout;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.widget.Button;
-
-
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,14 +27,61 @@ public class MainActivity extends AppCompatActivity {
     Button decode;
     Button education;
     ImageButton settings;
+    ImageButton encode1;
+    KeyGenerator keyGenerator;
+    SecretKey secretKey;
+
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(layout.activity_main);
 
+        Toolbar myToolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+        //GENERATING KEYS TO PASS TO ENCODE AND DECODE
+
+
+        //Generating Keys for Asymmetric Encryption
+        // Step 2: Create a KeyGenerator object
+        KeyPairGenerator keyPairGenerator = null;
+        try {
+            keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+        // Step 3: Initialize the KeyGenerator with a certain keysize
+        keyPairGenerator.initialize(512);
+        // Step 4: Generate the key pairs
+        KeyPair keyPair = keyPairGenerator.generateKeyPair();
+        // Step 5: Extract the keys
+        PrivateKey privateKey = keyPair.getPrivate();
+        PublicKey publicKey = keyPair.getPublic();
+
+        try {
+            keyGenerator = KeyGenerator.getInstance("AES");
+            keyGenerator.init(256);
+            secretKey = keyGenerator.generateKey();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+
+
         encode = findViewById(id.EncodeButton);
         encode.setOnClickListener(
+                v -> {
+                    Intent i = new Intent(MainActivity.this,encode_screen.class);
+                    i.putExtra("KeyPair", keyPair);
+                    i.putExtra("AESKey", secretKey);
+                    startActivity(i);
+                }
+        );
+
+        encode1 = findViewById(id.EncodingScreen);
+        encode1.setOnClickListener(
                 v -> {
                     Intent i = new Intent(MainActivity.this,encode_screen.class);
                     startActivity(i);
@@ -36,6 +92,8 @@ public class MainActivity extends AppCompatActivity {
         decode.setOnClickListener(
                 v -> {
                     Intent i = new Intent(MainActivity.this,decode_screen.class);
+                    i.putExtra("KeyPair", keyPair);
+                    i.putExtra("AESKey", secretKey);
                     startActivity(i);
                 }
         );
@@ -52,10 +110,10 @@ public class MainActivity extends AppCompatActivity {
         settings.setOnClickListener(
                 v -> {
                     Intent i = new Intent(MainActivity.this,settings_screen.class);
+                    i.putExtra("KeyPair", keyPair);
+                    i.putExtra("AESKey", secretKey);
                     startActivity(i);
                 }
         );
-
-
     }
 }
