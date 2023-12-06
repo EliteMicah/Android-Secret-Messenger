fe1f1ed19680ddf3941b55962603abd4c4d3c10c
Micah Woodring <84886282+EliteMicah@users.noreply.github.com>
2023-12-06 12:33:55 -0800

    Create HighEducation.java

---
+++ java/HighEducation.java
@@ -0,0 +1,81 @@
+package com.example.secret_messenger;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.MenuItem;
+import android.widget.Spinner;
+import android.widget.ArrayAdapter;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.ImageButton;
+
+import java.util.Objects;
+
+
+public class HighEducation extends AppCompatActivity {
+
+    ImageButton home;
+    ImageButton settings;
+    ImageButton encoding;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.highleveleducation);
+
+        // Back Button Toolbar
+        androidx.appcompat.widget.Toolbar myToolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+
+
+        //HOME BUTTON
+        home = findViewById(R.id.homeIcon);
+        home.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent i = new Intent(HighEducation.this,MainActivity.class);
+                        startActivity(i);
+                    }
+                }
+        );
+
+        //HOME BUTTON
+        encoding = findViewById(R.id.EncodingScreen);
+        encoding.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent i = new Intent(HighEducation.this,encode_screen.class);
+                        startActivity(i);
+                    }
+                }
+        );
+
+        settings = findViewById(R.id.SettingsButton);
+        settings.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent i = new Intent(HighEducation.this,settings_screen.class);
+                        startActivity(i);
+                    }
+                }
+        );
+
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        // If we got here, the user's action was not recognized.
+        // Invoke the superclass to handle it.
+        return super.onOptionsItemSelected(item);
+    }
+}
+
